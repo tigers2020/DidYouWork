d4b60311c8879206fc1c1be6e55093bf60f4ea70
tigers2020 <tigers2020@gmail.com>
2017-05-09 07:42:58 -0400

    feat: tag indicator added.

--- app/src/main/java/com/androidnerdcolony/didyouwork/activities/CreateProjectActivity.java
+++ app/src/main/java/com/androidnerdcolony/didyouwork/activities/CreateProjectActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidnerdcolony.didyouwork.R;
 
@@ -24,9 +24,14 @@ public class CreateProjectActivity extends AppCompatActivity {
     EditText tagsEditView;
     @BindView(R.id.tag_list)
     LinearLayout tagListView;
+    @BindView(R.id.project_profit)
+            TextView projectProfitView;
+    @BindView(R.id.time_per_day)
+            TextView projectTimePerDayView;
 
     Context context;
     boolean stringDel = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,19 +40,22 @@ public class CreateProjectActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        tagsEditView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_COMMA | keyCode == KeyEvent.KEYCODE_SEMICOLON | keyCode == KeyEvent.KEYCODE_SPACE)
-                {
-                    stringDel = true;
-                }else {
-                    stringDel = false;
-                }
-                return false;
-            }
+        setTagClipper();
+        setProjectProfitDialog();
 
+
+    }
+
+    private void setProjectProfitDialog() {
+        projectProfitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "profit clicked", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    private void setTagClipper() {
         tagsEditView.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -57,20 +65,25 @@ public class CreateProjectActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
+                if (s.length() != 0) {
+                    stringDel = s.charAt(s.length() - 1) == ';' || s.charAt(s.length() - 1) == ' ' || s.charAt(s.length() - 1) == ',' || s.charAt(s.length()-1) == '.';
+                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (stringDel){
+                if (stringDel && s.length() != 0) {
                     TextView textView = new TextView(context);
-                    textView.setText(s);
+                    String tag = s.toString();
+                    tag = tag.subSequence(0, tag.length()-1).toString();
+                    textView.setText(tag);
                     tagListView.addView(textView);
-                    tagsEditView.setText("");
+                    tagsEditView.getText().clear();
                     stringDel = false;
                 }
+
             }
         });
-
 
     }
 }
